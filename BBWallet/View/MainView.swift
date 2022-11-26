ad6fd2c4d1e45f6b515a3e4250096e9c0cce3a69
Spich <spichakovda@gmail.com>
2022-11-26 23:01:38 +0300

    Build 3

--- BBWallet/View/MainView.swift
+++ BBWallet/View/MainView.swift
@@ -12,25 +12,19 @@ struct MainView: View {
     // MARK: PROPERTIES
     @State private var selection: String = "home"
     @State var tabSelection: TabBarItem =  .home
-    var y = 50
     
     var body: some View {
         CustomTabBarContainerView(selection: $tabSelection) {
             HomeView()
                 .tabBarItem(tab: .home, selection: $tabSelection)
-//                .frame(maxWidth: UIScreen.main.bounds.width, maxHeight: UIScreen.main.bounds.height)
             WalletView()
                 .tabBarItem(tab:  .wallet, selection: $tabSelection)
-//                .frame(maxWidth: UIScreen.main.bounds.width, maxHeight: UIScreen.main.bounds.height)
             RewardsCenter()
                 .tabBarItem(tab: .rewards, selection: $tabSelection)
-//                .frame(maxWidth: UIScreen.main.bounds.width, maxHeight: UIScreen.main.bounds.height)
             Browser()
                 .tabBarItem(tab: .browser, selection: $tabSelection)
-//                .frame(maxWidth: UIScreen.main.bounds.width, maxHeight: UIScreen.main.bounds.height)
             ProfileView()
                 .tabBarItem(tab: .profile, selection: $tabSelection)
-//                .frame(maxWidth: UIScreen.main.bounds.width, maxHeight: UIScreen.main.bounds.height)
         }
         .navigationBarHidden(true)
     }
